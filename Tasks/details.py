49112aab102c12196f5f03379128f06249d3dd29
dteror <elvisteemzola@gmail.com>
2025-09-10 06:43:51 +0200

    Add files via upload

---
+++ Tasks/details.py
@@ -0,0 +1,11 @@
+#***Get user Name
+name = input("Enter Your name : ")
+#***Get user Age 
+age = input("Enter user age : ")
+#Get user house number 
+house_number = input("Enter house number : ")
+#Get user street number
+street_number = input("Enter street number : ")
+
+#Output a formatted string
+print(f"Hey my name is {name}, I am {age} years old , i stay in house {house_number} at {street_number} Morogoro Drive 1632 ")
