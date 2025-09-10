1a31dabdb349fc8e4f5ea50372f000221e5f6fa1
dteror <elvisteemzola@gmail.com>
2025-09-10 06:14:05 +0200

    Update hello_world.py

--- Tasks/hello_word.py
+++ Tasks/hello_word.py
@@ -1,1 +1,16 @@
 
+#****Pseudo code****#
+#1.Get user_name 
+#2. output user_name
+#3. Get user_age
+#4. Output user_age
+#5. Output "hello world"
+
+#Requesting user for name 
+user_name = input("Enter your name : ")
+print(user_name)
+#Requesting user for their Age
+user_age = input("Enter your age : ")
+print(user_age)
+#Print hello world
+print("Hello world")
