60476968ae3bf973d3bde13d6bcb2d16466b8912
dteror <elvisteemzola@gmail.com>
2025-09-10 07:04:04 +0200

    Add files via upload

---
+++ Tasks/conversion.py
@@ -0,0 +1,15 @@
+
+#Declaring  the following variables num1, num2, num3, string1:
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = '100'
+#Converting num1 into an integer
+print(int(num1))
+#Converting and printing num2 as a float
+print(float(num2))
+#Converting and printing num3 as a string
+print(str(num3))
+#Converting and printing string1 as integer
+print(int(string1))
+
